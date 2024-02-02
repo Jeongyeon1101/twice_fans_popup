2660807ea40b51c0ba9804a3de884b9fd52ab91b
MegumiSaito <twice1101oncemegumi@gmail.com>
2024-02-02 15:10:21 +0000

    [Finish]devise,

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  #管理者用
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  #顧客用
+  devise_for :customers, skip: [:passwords], controllers: {
+    resistrations: "public/resistrations",
+    sessions: "public/sessionss"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
