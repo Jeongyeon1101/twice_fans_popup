660fd5abe1503a7285728cc68c4118d1cf0b44c2
MegumiSaito <twice1101oncemegumi@gmail.com>
2024-02-03 15:44:01 +0000

    [Add]サインイン,サインアウト後の処理(adminとcustomerの区別)

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,13 @@ Rails.application.routes.draw do
     resistrations: "public/resistrations",
     sessions: "public/sessionss"
   }
+
+  namespace :admin do
+    root to: "homes#top"
+  end
+
+  scope module: :public do
+    root to: "homes#top"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
