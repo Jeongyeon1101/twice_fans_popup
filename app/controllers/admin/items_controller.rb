403db9e3f0b92b096ea33d116ddfc7dd3a7b55e5
MegumiSaito <twice1101oncemegumi@gmail.com>
2024-02-10 12:07:12 +0000

    [Finsh]商品新規作成・一覧機能作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,3 +1,30 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    #新しいitemを保存する
+    if @item.save
+      #成功したらリダイレクトする
+      redirect_to admin_item_path(@item)
+    else
+      #失敗したら新規登録画面をrender(上書き表示)する
+      render :new
+    end
+  end
+
+  def index
+    @items = Item.all
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+  end
 end
